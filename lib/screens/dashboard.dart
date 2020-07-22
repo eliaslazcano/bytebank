743b62befd96d19fcf5c342d433488d3a60459bc
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-07-22 12:54:44 -0400

    Grid e alinhamentos do Dashboard
    A estrutura foi re-desenhada para ficar adequado ao layout proposto pelo projeto. Utilizamos Container para colocar conteudos na tela, e Column para alinhar os conteudos. O alinhamento do Column é semelhante ao do Flexbox do CSS3.

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -7,10 +7,39 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
-      body: Column(
-        children: <Widget>[
-          Image.asset("images/bytebank_logo.png")
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, //Alinhamento vertical
+          crossAxisAlignment: CrossAxisAlignment.start, //Alinhamento horizontal
+          children: <Widget>[
+            Image.asset("images/bytebank_logo.png"),
+            Container(
+              color: Theme.of(context).primaryColor, //Cor de fundo, usando a cor primaria do tema. Aceita cores da classe Colors.cor tambem.
+              height: 100,
+              width: 120,
+              padding: EdgeInsets.all(8),
+              child: Column( // Filhos
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 48,
+                  ),
+                  Text(
+                    "Contatos",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
