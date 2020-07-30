e41c713d4f064c1d1cec78a5408410ba72fcf33b
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-07-30 18:58:21 -0400

    webclient.dart agora é uma classe
    Convertido para classe, contendo no momento apenas uma função onde é possível obter a listagem de "Transferencias" que ficam armazenadas em um servidor remoto.

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -5,28 +5,23 @@ import 'package:bytebank/models/Transferencia.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:http/http.dart';
 
-//URL base para as requisições
-const String host = "http://localhost:8080";
+class Webclient {
+  //URL base para as requisições
+  static const String host = "http://192.168.15.101:8080";
 
-//Realiza uma requisição GET.
-Future<void> requisicaoGet() async {
-  final Response response = await get('$host/transactions');
-  print(response.body);
-}
+  Future<List<Transferencia>> obterTransferencias() async {
+    final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+    final Response response = await client.get('$host/transactions');
 
-//Realiza uma requisição GET com interceptador.
-Future<List<Transferencia>> requisicaoGetComInterceptor() async {
-  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client.get('$host/transactions');
-
-  //Adapta o JSON para nossos objetos modelados.
-  final List<dynamic> decodedJson = jsonDecode(response.body);
-  final List<Transferencia> transferencias = List();
-  for (Map<String, dynamic> transferenciaJson in decodedJson) {
-    final Map<String, dynamic> contatoJson = transferenciaJson['contact'];
-    final Contato contato = Contato(contatoJson['name'], null, contatoJson['accountNumber']);
-    final Transferencia transferencia = Transferencia(contato, transferenciaJson['value']);
-    transferencias.add(transferencia);
+    //Adapta o JSON para um List<Transferencia>.
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+    final List<Transferencia> transferencias = List();
+    for (Map<String, dynamic> transferenciaJson in decodedJson) {
+      final Map<String, dynamic> contatoJson = transferenciaJson['contact'];
+      final Contato contato = Contato(contatoJson['name'], null, contatoJson['accountNumber']);
+      final Transferencia transferencia = Transferencia(contato, transferenciaJson['value']);
+      transferencias.add(transferencia);
+    }
+    return transferencias;
   }
-  return transferencias;
 }
