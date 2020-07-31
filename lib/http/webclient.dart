768912d2478c6f0f39467411f7bebbc2ec4a1704
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-07-31 02:26:00 -0400

    Utilizando a Web API na aplicação.

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -7,11 +7,13 @@ import 'package:http/http.dart';
 
 class Webclient {
   //URL base para as requisições
-  static const String host = "http://192.168.15.101:8080";
+  static const String baseUrl = "http://192.168.15.101:8080";
+  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
 
   Future<List<Transferencia>> obterTransferencias() async {
-    final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-    final Response response = await client.get('$host/transactions');
+    final Response response = await client
+        .get('$baseUrl/transactions')
+        .timeout(Duration(seconds: 60));
 
     //Adapta o JSON para um List<Transferencia>.
     final List<dynamic> decodedJson = jsonDecode(response.body);
@@ -23,5 +25,35 @@ class Webclient {
       transferencias.add(transferencia);
     }
     return transferencias;
+  }
+
+  Future<Transferencia> salvarTransferencia(Transferencia transferencia) async {
+    //Map<String, dynamic> => Mapa (igual array) de chave String e valor dynamic (tipagem variavel)
+    final Map<String, dynamic> transferenciaMap = {
+      'value': transferencia.valor,
+      'contact': {
+        'name': transferencia.contato.nome,
+        'accountNumber': transferencia.contato.conta
+      }
+    };
+    final String tranferenciaJson = jsonEncode(transferenciaMap);
+
+    final Response response = await client
+        .post('$baseUrl/transactions',
+            headers: {
+              //O uso de {chave: dado} será auto-convertido para um Map<String>
+              'Content-type': 'application/json',
+              'password': '1000',
+            },
+            body: tranferenciaJson)
+        .timeout(Duration(seconds: 60));
+
+    //Trata a resposta
+    Map<String, dynamic> json = jsonDecode(response.body);
+    final Map<String, dynamic> contatoJson = json['contact'];
+    return Transferencia(
+      Contato(contatoJson['name'], null, contatoJson['accountNumber']),
+      0
+    );
   }
 }
--- lib/screens/lista_transferencias.dart
+++ lib/screens/lista_transferencias.dart
@@ -1,5 +1,7 @@
+import 'package:bytebank/components/carregando_centralizado.dart';
 import 'package:bytebank/components/item_transferencia.dart';
-import 'package:bytebank/database/dao/transferencia_dao.dart';
+import 'package:bytebank/components/mensagem_centralizada.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/Transferencia.dart';
 import 'package:bytebank/screens/lista_contatos.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +14,7 @@ class ListaTransferencias extends StatefulWidget {
 }
 
 class _ListaTransferenciasState extends State<ListaTransferencias> {
-  final TransferenciaDao dao = TransferenciaDao();
+  final Webclient webclient = Webclient();
 
   @override
   Widget build(BuildContext context) {
@@ -22,33 +24,39 @@ class _ListaTransferenciasState extends State<ListaTransferencias> {
           title: Text('Transferencias'),
         ),
         body: FutureBuilder(
-          future: dao.listarTodos(),
+          future: webclient.obterTransferencias(),
           builder: (context, snapshot) {
             switch(snapshot.connectionState) {
               case ConnectionState.none:
-              // Não está em execução/não foi executado.
+                // Não está em execução/não foi executado.
                 break;
               case ConnectionState.active:
-              // Future em execução, mas já tem dado disponível (Futures do tipo Stream)
+                // Future em execução, mas já tem dado disponível (Futures do tipo Stream)
                 break;
               case ConnectionState.waiting:
-              // Future em execução
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
+                // Future em execução
+                return CarregandoCentralizado();
                 break;
               case ConnectionState.done:
-              // Future concluído.
-                final List<Transferencia> transferencias = snapshot.data;
-                return ListView.builder(
-                  itemCount: transferencias.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ItemTransferencia(transferencias[index]);
-                  },
+                // Future concluído.
+                if (snapshot.hasData) { //Filtra erro da API
+                  final List<Transferencia> transferencias = snapshot.data;
+                  if (transferencias.isNotEmpty) {
+                    return ListView.builder(
+                      itemCount: transferencias.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ItemTransferencia(transferencias[index]);
+                      },
+                    );
+                  }
+                }
+                return MensagemCentralizada(
+                  "Nenhuma transação encontrada",
+                  icone: Icons.warning,
                 );
                 break;
             }
-            return Text('Unknown error');
+            return MensagemCentralizada("Erro desconhecido");
           },
         ),
         floatingActionButton: FloatingActionButton(
